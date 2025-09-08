515aee87eb736adec5af79c11780c4248e0faf8b
Leo <leo@wilkin.xyz>
2025-09-08 23:41:13 +0100

    v1.1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,37 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Unofficial UKSDC Docs',
-			social: [{ icon: 'open-book', label: 'UKSDC Homepage', href: 'https://uksdc.org/' }],
-			sidebar: [
-				{
-					label: 'Prime Contractors',
-					autogenerate: { directory: 'prime-contractors' },
-				},
-				{
-					label: 'Subcontractors',
-					autogenerate: { directory: 'subcontractors' },
-				},
-				{
-					label: 'RFP Sections',
-					autogenerate: { directory: 'rfp' },
-				},
-				{
-					label: 'The Foundation Society',
-					autogenerate: { directory: 'foundation-society' },
-				}
-			],
-		}),
-	],
+    site: 'https://uksdc.leowilkin.com',
+    integrations: [starlight({
+        title: 'Unofficial UKSDC Docs',
+		editLink: {
+			baseUrl: 'https://github.com/leowilkin/uksdc-docs/edit/main/',
+		},
+        logo: {
+            light: './src/assets/light.png',
+            dark: './src/assets/dark.png',
+        },
+        social: [{ icon: 'open-book', label: 'UKSDC Homepage', href: 'https://uksdc.org/' }],
+        sidebar: [
+            {
+                label: 'Prime Contractors',
+                autogenerate: { directory: 'prime-contractors' },
+            },
+            {
+                label: 'Subcontractors',
+                autogenerate: { directory: 'subcontractors' },
+            },
+            {
+                label: 'RFP Sections',
+                autogenerate: { directory: 'rfp' },
+            },
+            {
+                label: 'The Foundation Society',
+                autogenerate: { directory: 'foundation-society' },
+            }
+        ],
+		}), sitemap()],
 });
